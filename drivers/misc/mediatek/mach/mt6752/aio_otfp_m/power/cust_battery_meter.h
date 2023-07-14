16a762d5b6ccc1643bb0abf881a20432f54ff734
howetuft <howetuft@gmail.com>
2023-07-14 19:36:28 +0200

    power cust_*

--- drivers/misc/mediatek/mach/mt6752/aio_otfp_m/power/cust_battery_meter.h
+++ drivers/misc/mediatek/mach/mt6752/aio_otfp_m/power/cust_battery_meter.h
@@ -9,10 +9,12 @@
 //#define SOC_BY_AUXADC
 #define SOC_BY_HW_FG
 //#define SOC_BY_SW_FG
-//#define HW_FG_FORCE_USE_SW_OCV
+#define HW_FG_FORCE_USE_SW_OCV
 
 //#define CONFIG_DIS_CHECK_BATTERY
 //#define FIXED_TBAT_25
+
+#define CUST_DISABLE_CAPACITY_OCV2CV_TRANSFORM
 
 /* ADC Channel Number */
 #if 0
@@ -34,21 +36,19 @@
 #define TEMPERATURE_T3             50
 #define TEMPERATURE_T              255 // This should be fixed, never change the value
 
-//lenovo-sw mahj2 modify Begin
-#define FG_METER_RESISTANCE 	50
-//lenovo-sw mahj2 modify End
-/* Begin, lenovo-sw mahj2 update battery param at 20141021*/
-/* Qmax for battery  */
-#define Q_MAX_POS_50	2798
-#define Q_MAX_POS_25	2777
-#define Q_MAX_POS_0		2518
-#define Q_MAX_NEG_10	1698
+#define FG_METER_RESISTANCE 	0
 
-#define Q_MAX_POS_50_H_CURRENT	2796
-#define Q_MAX_POS_25_H_CURRENT	2761
-#define Q_MAX_POS_0_H_CURRENT	2489
-#define Q_MAX_NEG_10_H_CURRENT	1681
-/* End, lenovo-sw mahj2 update battery param at 20141021*/
+/* Qmax for battery  */
+#define Q_MAX_POS_50	8884
+#define Q_MAX_POS_25	8884
+#define Q_MAX_POS_0 	8884
+#define Q_MAX_NEG_10	8884
+
+#define Q_MAX_POS_50_H_CURRENT	8707
+#define Q_MAX_POS_25_H_CURRENT	8707
+#define Q_MAX_POS_0_H_CURRENT 	8707
+#define Q_MAX_NEG_10_H_CURRENT	8707
+
 
 /* Discharge Percentage */
 #define OAM_D5		 1		//  1 : D5,   0: D2
@@ -56,18 +56,8 @@
 
 /* battery meter parameter */
 #define CHANGE_TRACKING_POINT
-/*Begin, lenovo-sw mahj2 modify at 20141105 */
-//#define CUST_TRACKING_POINT  1
-#define CUST_TRACKING_POINT  20
-/*End, lenovo-sw mahj2 modify at 20141105 */
-/*Begin, lenovo-sw mahj2 modify for cust r sense at 20141011 */
-//#define CUST_R_SENSE         68
-#ifdef CONFIG_MTK_BQ24157_SUPPORT
-#define CUST_R_SENSE         56 //33mohm
-#else
-#define CUST_R_SENSE         33 //33mohm
-#endif
-/*end, lenovo-sw mahj2 modify for cust r sense at 20141011 */
+#define CUST_TRACKING_POINT  1
+#define CUST_R_SENSE         68
 #define CUST_HW_CC 		    0
 #define AGING_TUNING_VALUE   103
 #define CUST_R_FG_OFFSET    0
@@ -75,10 +65,7 @@
 #define OCV_BOARD_COMPESATE	0 //mV 
 #define R_FG_BOARD_BASE		1000
 #define R_FG_BOARD_SLOPE	1000 //slope
-/*Begin, lenovo-sw mahj2 modify at 20141105*/
-//#define CAR_TUNE_VALUE		97 //1.00
-#define CAR_TUNE_VALUE		103 //1.00
-/*end, lenovo-sw mahj2 modify at 20141105 */
+#define CAR_TUNE_VALUE		80 //1.00
 
 
 /* HW Fuel gague  */
@@ -86,30 +73,29 @@
 #define MinErrorOffset       1000
 #define FG_VBAT_AVERAGE_SIZE 18
 #define R_FG_VALUE 			10 // mOhm, base is 20
-/* lenovo-sw zhangrc2 change tolrance from 25 to 40 2014-11-06 */
+
 #define CUST_POWERON_DELTA_CAPACITY_TOLRANCE	40
-#define CUST_POWERON_LOW_CAPACITY_TOLRANCE		2
-/* lenovo-sw zhangrc2 change tolrance from 25 to 40 2014-11-06 */
+#define CUST_POWERON_LOW_CAPACITY_TOLRANCE		5
 #define CUST_POWERON_MAX_VBAT_TOLRANCE			90
 #define CUST_POWERON_DELTA_VBAT_TOLRANCE		30
 
 /* Disable Battery check for HQA */
-#ifdef CONFIG_MTK_DISABLE_POWER_ON_OFF_VOLTAGE_LIMITATION
+#ifdef MTK_DISABLE_POWER_ON_OFF_VOLTAGE_LIMITATION
 #define FIXED_TBAT_25
 #endif
 
 /* Dynamic change wake up period of battery thread when suspend*/
-#define VBAT_NORMAL_WAKEUP		3700		//3.7V
-#define VBAT_LOW_POWER_WAKEUP		3550		//3.55v
-#define NORMAL_WAKEUP_PERIOD		2700		//45 * 60 = 90 min
-#define LOW_POWER_WAKEUP_PERIOD		240		//4* 60 = 4 min
+#define VBAT_NORMAL_WAKEUP		3600		//3.6V
+#define VBAT_LOW_POWER_WAKEUP		3500		//3.5v
+#define NORMAL_WAKEUP_PERIOD		5400 		//90 * 60 = 90 min
+#define LOW_POWER_WAKEUP_PERIOD		300		//5 * 60 = 5 min
 #define CLOSE_POWEROFF_WAKEUP_PERIOD	30	//30 s
 
 #define INIT_SOC_BY_SW_SOC
 //#define SYNC_UI_SOC_IMM			//3. UI SOC sync to FG SOC immediately
-//#define MTK_ENABLE_AGING_ALGORITHM	//6. Q_MAX aging algorithm
+#define MTK_ENABLE_AGING_ALGORITHM	//6. Q_MAX aging algorithm
 #define MD_SLEEP_CURRENT_CHECK	//5. Gauge Adjust by OCV 9. MD sleep current check
-#define Q_MAX_BY_CURRENT		//7. Qmax varient by current loading.
+//#define Q_MAX_BY_CURRENT		//7. Qmax varient by current loading.
 
 #define DISABLE_RFG_EXIST_CHECK
 #endif	//#ifndef _CUST_BATTERY_METER_H
--- drivers/misc/mediatek/mach/mt6752/aio_otfp_m/power/cust_charging.h
+++ drivers/misc/mediatek/mach/mt6752/aio_otfp_m/power/cust_charging.h
@@ -2,7 +2,7 @@
 #define _CUST_BAT_H_
 
 /* stop charging while in talking mode */
-//#define STOP_CHARGING_IN_TAKLING
+#define STOP_CHARGING_IN_TAKLING
 #define TALKING_RECHARGE_VOLTAGE 3800
 #define TALKING_SYNC_TIME		   60
 
@@ -27,35 +27,19 @@
 #define USB_CHARGER_CURRENT_CONFIGURED		CHARGE_CURRENT_500_00_MA	// 500mA
 
 #define USB_CHARGER_CURRENT					CHARGE_CURRENT_500_00_MA	//500mA
-//#define AC_CHARGER_CURRENT					CHARGE_CURRENT_650_00_MA
-/*Begin, lenovo-sw wangxf14 modify for 2A charge at 20140903 */
-#define AC_CHARGER_CURRENT				CHARGE_CURRENT_2050_00_MA
+#if defined(MTK_BQ24158_SUPPORT) || defined(MTK_FAN5405_SUPPORT)
+#define AC_CHARGER_CURRENT					CHARGE_CURRENT_1250_00_MA //1.25A
+#elif defined(MTK_BQ24196_SUPPORT) || defined(MTK_BQ24296_SUPPORT) || defined(MTK_BQ24261_SUPPORT)
+#define AC_CHARGER_CURRENT					CHARGE_CURRENT_2000_00_MA //2.0A, max charging 2.5A
+#else
+#define AC_CHARGER_CURRENT					CHARGE_CURRENT_1000_00_MA
+#endif
 #define NON_STD_AC_CHARGER_CURRENT			CHARGE_CURRENT_500_00_MA
-/*End, lenovo-sw wangxf14 modify for 2A charge at 20140903 */
-#define CHARGING_HOST_CHARGER_CURRENT       CHARGE_CURRENT_1500_00_MA
+#define CHARGING_HOST_CHARGER_CURRENT       CHARGE_CURRENT_650_00_MA
 #define APPLE_0_5A_CHARGER_CURRENT          CHARGE_CURRENT_500_00_MA
-#define APPLE_1_0A_CHARGER_CURRENT          CHARGE_CURRENT_1000_00_MA
-#define APPLE_2_1A_CHARGER_CURRENT          CHARGE_CURRENT_1000_00_MA
-/*Begin, lenovo-sw mahj2 modify for current limit at 20141113 */
-#define AC_CHARGER_CURRENT_LIMIT	              CHARGE_CURRENT_900_00_MA   //lenovo standard 0.3C
-/*End, lenovo-sw mahj2 modify for current limit at 20141113 */
-/*Begin, lenovo-sw mahj2 modify for input current limit at 20150106 */
-#define INPUT_CHARGER_CURRENT_LIMIT	              CHARGE_CURRENT_1800_00_MA
-/*End, lenovo-sw mahj2 modify for input current limit at 20150106 */
+#define APPLE_1_0A_CHARGER_CURRENT          CHARGE_CURRENT_650_00_MA
+#define APPLE_2_1A_CHARGER_CURRENT          CHARGE_CURRENT_800_00_MA
 
-/*Begin, lenovo-sw chailu1 add for 45-50  0.5c   20150318 */
-#define LENOVO_AC_CHARGER_CURRENT_LIMIT_HTEMP_SUPPORT
-#ifdef LENOVO_AC_CHARGER_CURRENT_LIMIT_HTEMP_SUPPORT
-#define AC_CHARGER_CURRENT_LIMIT_HTEMP	    CHARGE_CURRENT_1500_00_MA//0.5c
-#endif
-/*End, lenovo-sw chailu1 add for 45-50  0.5c   20150318 */
-
-/*Begin, lenovo-sw chailu1 add 45-50  CV limit fuction, use 3rd fg  20150318 */
-#define LENOVO_TEMP_POS_45_TO_POS_50_CV_LiMIT_SUPPORT
-#ifdef LENOVO_TEMP_POS_45_TO_POS_50_CV_LiMIT_SUPPORT
-#define LENOVO_TEMP_POS_45_TO_POS_50_CV_VOLTAGE		BATTERY_VOLT_04_140000_V	   
-#endif
-/*End, lenovo-sw chailu1 add 45-50  CV limit fuction, use 3rd fg  20150318 */
 
 /* Precise Tunning */
 #define BATTERY_AVERAGE_DATA_NUMBER	3	
@@ -68,12 +52,10 @@
 #define V_CHARGER_MIN 4400				// 4.4 V
 
 /* Tracking TIME */
-//lenovo-sw mahj2 modify for 100% tracking time Begin
-#define ONEHUNDRED_PERCENT_TRACKING_TIME	60	// 60 second
-//lenovo-sw mahj2 modify for 100% tracking time End
+#define ONEHUNDRED_PERCENT_TRACKING_TIME	10	// 10 second
 #define NPERCENT_TRACKING_TIME	   			20	// 20 second
 #define SYNC_TO_REAL_TRACKING_TIME  		60	// 60 second
-#define V_0PERCENT_TRACKING							3450 //3450mV
+#define V_0PERCENT_TRACKING							3500 //3450mV
 
 /* Battery Notify */
 #define BATTERY_NOTIFY_CASE_0001_VCHARGER
@@ -82,30 +64,17 @@
 //#define BATTERY_NOTIFY_CASE_0004_VBAT
 //#define BATTERY_NOTIFY_CASE_0005_TOTAL_CHARGINGTIME
 
-/* High battery support */
-//lenovo-sw mahj2 modify for support hight battery vol Begin
-#define HIGH_BATTERY_VOLTAGE_SUPPORT
-//lenovo-sw mahj2 modify for support hight battery vol End
-
 /* JEITA parameter */
 //#define MTK_JEITA_STANDARD_SUPPORT
 #define CUST_SOC_JEITA_SYNC_TIME 30
 #define JEITA_RECHARGE_VOLTAGE  4110	// for linear charging
-#ifdef HIGH_BATTERY_VOLTAGE_SUPPORT
-#define JEITA_TEMP_ABOVE_POS_60_CV_VOLTAGE		BATTERY_VOLT_04_240000_V
-#define JEITA_TEMP_POS_45_TO_POS_60_CV_VOLTAGE		BATTERY_VOLT_04_240000_V
-#define JEITA_TEMP_POS_10_TO_POS_45_CV_VOLTAGE		BATTERY_VOLT_04_340000_V
-#define JEITA_TEMP_POS_0_TO_POS_10_CV_VOLTAGE		BATTERY_VOLT_04_240000_V
-#define JEITA_TEMP_NEG_10_TO_POS_0_CV_VOLTAGE		BATTERY_VOLT_04_040000_V
-#define JEITA_TEMP_BELOW_NEG_10_CV_VOLTAGE		BATTERY_VOLT_04_040000_V
-#else
 #define JEITA_TEMP_ABOVE_POS_60_CV_VOLTAGE		BATTERY_VOLT_04_100000_V
 #define JEITA_TEMP_POS_45_TO_POS_60_CV_VOLTAGE	BATTERY_VOLT_04_100000_V
 #define JEITA_TEMP_POS_10_TO_POS_45_CV_VOLTAGE	BATTERY_VOLT_04_200000_V
 #define JEITA_TEMP_POS_0_TO_POS_10_CV_VOLTAGE	BATTERY_VOLT_04_100000_V
 #define JEITA_TEMP_NEG_10_TO_POS_0_CV_VOLTAGE	BATTERY_VOLT_03_900000_V
 #define JEITA_TEMP_BELOW_NEG_10_CV_VOLTAGE		BATTERY_VOLT_03_900000_V
-#endif
+
 /* For JEITA Linear Charging only */
 #define JEITA_NEG_10_TO_POS_0_FULL_CURRENT  120	//mA 
 #define JEITA_TEMP_POS_45_TO_POS_60_RECHARGE_VOLTAGE  4000
@@ -118,38 +87,36 @@
 #define JEITA_TEMP_NEG_10_TO_POS_0_CC2TOPOFF_THRESHOLD	3850
 
 
-/*lenovo-sw weiweij added for charging terminate as 0.1c*/
-#define LENOVO_CHARGING_TERM
-#ifdef LENOVO_CHARGING_TERM
-#define LENOVO_CHARGING_TERM_CUR_STAGE_1	7	//275ma
-#define LENOVO_CHARGING_TERM_CUR_STAGE_2	2	//150ma
-#endif
-/*lenovo-sw weiweij added for charging terminate as 0.1c end*/
+/* High battery support */
+#define HIGH_BATTERY_VOLTAGE_SUPPORT
 
-#ifdef CONFIG_LENOVO_POWEROFF_CHARGING_UI
-//draw
-#define LENOVO_CHARGING_DRAW_LEFT                 (540-144) // percent number_left + 2*number_width
-#define LENOVO_CHARGING_DRAW_RIGHT                (LENOVO_CHARGING_DRAW_LEFT+288)
-#define LENOVO_CHARGING_DRAW_BOTTOM               (1920-40)
-#define LENOVO_CHARGING_DRAW_TOP                  (LENOVO_CHARGING_DRAW_BOTTOM-108)
-#endif
-
-/* lenovo-sw zhangrc2 use pmic to control charging led 2014-12-08 */
-#define LENOVO_USE_PMIC_CONTROL_LED
-/* lenovo-sw zhangrc2 use pmic to control charging led 2014-12-08 */
-/*lenovo-sw mahj2 added for ntc temp cut 2 degree Begin*/
-//#define LENOVO_NTC_TEMP_CUT_2_DEGREE
-/*lenovo-sw mahj2 added for ntc temp cut 2 degree End*/
 /* For CV_E1_INTERNAL */
 #define CV_E1_INTERNAL
 
 /* Disable Battery check for HQA */
-#ifdef CONFIG_MTK_DISABLE_POWER_ON_OFF_VOLTAGE_LIMITATION
+#ifdef MTK_DISABLE_POWER_ON_OFF_VOLTAGE_LIMITATION
 #define CONFIG_DIS_CHECK_BATTERY
 #endif
 
-#ifdef CONFIG_MTK_FAN5405_SUPPORT
+#ifdef MTK_FAN5405_SUPPORT
 #define FAN5405_BUSNUM 1
+#endif
+
+/* Pump Express support (fast charging) */
+#ifdef MTK_PUMP_EXPRESS_PLUS_SUPPORT
+#define TA_START_BATTERY_SOC	1
+#define TA_STOP_BATTERY_SOC 	95
+#define TA_AC_9V_INPUT_CURRENT	CHARGE_CURRENT_1500_00_MA
+#define TA_AC_7V_INPUT_CURRENT	CHARGE_CURRENT_1500_00_MA
+#define TA_AC_CHARGING_CURRENT	CHARGE_CURRENT_2500_00_MA
+#define TA_9V_SUPPORT
+
+#undef V_CHARGER_MAX
+#ifdef TA_9V_SUPPORT
+#define V_CHARGER_MAX 10500				// 10.5 V
+#else
+#define V_CHARGER_MAX 7500				// 7.5 V
+#endif
 #endif
 
 #endif /* _CUST_BAT_H_ */ 
